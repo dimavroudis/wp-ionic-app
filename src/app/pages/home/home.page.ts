d8d554136cec88c4ee6cbe062b94144582c03331
Dimitris Mavroudis <im.dimitrismavroudis@gmail.com>
2020-01-10 00:49:42 +0200

    Add description in home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -22,8 +22,12 @@ export class HomePage implements OnInit {
 		this.post.getPosts().subscribe(async posts => {
 			this.latestPosts = posts;
 		});
-		this.post.getPosts({sticky: true}).subscribe(async posts => {
+		this.post.getPosts({ sticky: true }).subscribe(async posts => {
 			this.stickyPosts = posts;
+		});
+		this.settings.getAppInfo().subscribe(data => {
+			this.title = data.name;
+			this.description = data.description;
 		});
 	}
 
